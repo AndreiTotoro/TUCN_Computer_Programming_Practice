85d2da82982ac33460ed7f375df73a46d5573304
andreitotoro <andrei@ciocoiu.net>
2026-01-07 10:58:16 -0500

    Re-Organize

--- A/main.c
+++ A/main.c
@@ -12,8 +12,8 @@ int main(){
     read_secret_code("input_text_a.txt", secret_code);
     read_text("input_text_a.txt", text);
     mat = construct_code_matrix(secret_code, text, &row_num, &col_num);
-    write_code_matrix("output_text_a.txt", mat, row_num, col_num);
     headers = get_and_sort_mat_headers(mat, col_num);
+    write_code_matrix("output_text_a.txt", mat, row_num, col_num);
     write_encrypted_text("output_text_a.txt", mat, row_num, col_num, headers);
 
     free(headers);
